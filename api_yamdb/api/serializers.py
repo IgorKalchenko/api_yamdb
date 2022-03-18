f6aa3deee092c8821eda2ef160e3314e1c57522a
Григорий Шемякин <Shemyakin-Gr@yandex.ru>
2022-03-18 19:20:50 +0500

    180322-gr

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,59 @@
+import datetime as dt
+
+from django.db import models
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(
+        read_only=True,
+        many=False
+    )
+    genre = GenreSerializer(
+        read_only=True,
+        many=True
+    )
+
+    
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year',
+                  'genre', 'category')
+
+
+# class TitlePostSerializer(serializers.ModelSerializer):
+#     category = serializers.SlugRelatedField(
+#         queryset=Category.objects.all(),
+#         slug_field='slug'
+#     )
+
+#     genre = serializers.SlugRelatedField(
+#         queryset=Genre.objects.all(),
+#         slug_field='slug',
+#         many=True
+#     )
+
+#     class Meta:
+#         model = Title
+#         fields = ('id', 'name', 'year',
+#                   'genre', 'category')
+
+#     def validate_year(self, value):
+#         year = dt.datetime.today().year
+#         if value > year:
+#             raise serializers.ValidationError('Не правильно указан год')
+#         return value
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,130 @@
+import datetime as dt
 from django.db import models
 
-# Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Категория'
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name='Жанр'
+    )
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+    
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Наименование',
+        # unique=True, - в разных категориях могут повторяться
+        max_length=256
+    )
+    year = models.PositiveIntegerField(
+        verbose_name='Год выпуска',
+        validators=[MaxValueValidator(dt.datetime.today().year)]
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='titles'
+        )
+    genre = models.ManyToManyField(Genre, through='Genre_Title')
+
+    class Meta:
+        verbose_name = 'Наименование'
+        verbose_name_plural = 'Наименования'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre_Title(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    def __str__(self):
+        return f'{self.title} {self.genre}'
+
+# class Review(models.Model):
+#     title = models.ForeignKey(
+#         Title,
+#         on_delete=models.CASCADE
+#     )
+#     text = models.TextField()
+#     author = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE
+#     )
+#     score = models.PositiveSmallIntegerField(
+#         validators=[MinValueValidator(0),
+#                     MaxValueValidator(10)]
+#     )
+#     pub_date = models.DateTimeField(
+#         verbose_name='Дата публикации',
+#         auto_now_add=True,
+#         db_index=True
+#     )
+
+#     class Meta:
+#         ordering = ['-pub_date']
+#         verbose_name = 'Отзыв'
+#         verbose_name_plural = 'Отзывы'
+
+
+# class Commentary(models.Model):
+#     review = models.ForeignKey(
+#         Review, 
+#         on_delete=models.CASCADE,
+#     )
+#     text = models.TextField(
+#         null=True,
+#         blank=True
+#     )
+#     author = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE)
+#     pub_date = models.DateTimeField(
+#         verbose_name='Дата публикации',
+#         auto_now_add=True,
+#         db_index=True
+#     )
+
+#     class Meta:
+#         verbose_name = 'Комментарий'
+#         verbose_name_plural = 'Комментарии'
