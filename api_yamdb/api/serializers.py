5b26c6ee01a61d6dee6ad25f63415c193509619b
Rustam A <69015050+lauman302@users.noreply.github.com>
2022-03-20 13:39:39 +0300

    add serializer for comm/review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,8 @@
 import datetime as dt
 
-from django.db import models
 from rest_framework import serializers
 
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title, Comment, Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -28,32 +27,53 @@ class TitleSerializer(serializers.ModelSerializer):
         many=True
     )
 
-    
     class Meta:
         model = Title
         fields = ('id', 'name', 'year',
                   'genre', 'category')
 
 
-# class TitlePostSerializer(serializers.ModelSerializer):
-#     category = serializers.SlugRelatedField(
-#         queryset=Category.objects.all(),
-#         slug_field='slug'
-#     )
+class TitlePostSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
 
-#     genre = serializers.SlugRelatedField(
-#         queryset=Genre.objects.all(),
-#         slug_field='slug',
-#         many=True
-#     )
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
 
-#     class Meta:
-#         model = Title
-#         fields = ('id', 'name', 'year',
-#                   'genre', 'category')
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'genre', 'category'
+        )
 
-#     def validate_year(self, value):
-#         year = dt.datetime.today().year
-#         if value > year:
-#             raise serializers.ValidationError('Не правильно указан год')
-#         return value
+    def validate_year(self, value):
+        year = dt.datetime.today().year
+        if value > year:
+            raise serializers.ValidationError('Не правильно указан год')
+        return value
+
+
+class CommentSerializer(serializers.Serializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.Serializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        fields = ('__all__')
+        read_only_fields = ('title', )
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
 app_name = 'api'
+router = SimpleRouter()
+
+# router.register()
+
 
 urlpatterns = [
     path('v1/', include('djoser.urls')),
